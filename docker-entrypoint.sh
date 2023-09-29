89dd6d76bc2e7b1a66ed11acb0ed8921e352a010
bincooo <admin@bincooodeiMac.local>
2023-09-29 21:31:13 +0800

    new: docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo ' give me the star, thank ~'
+echo '+------------------------------------------------------+'
+echo '|               github: bincooo/worker-laf             |'
+echo '|                   please wait ...                    |'
+echo '+------------------------------------------------------+'
+echo ' '
+
+
+git clone https://github.com/bincooo/worker-laf.git /app/main
+cd /app/main
+pnpm i
+pnpm run dev -y
