6f268fb51d625211beb916b85137c382c65de2f6
折戟沉沙、丿 <36452456+bincooo@users.noreply.github.com>
2023-11-28 06:07:12 +0800

    Update ping.js

--- script/ping.js
+++ script/ping.js
@@ -1,1 +1,28 @@
+const 
+    request  = arguments[0],
+    response = arguments[1]
 
+const type = request.header('content-type') ?? ''
+if (type.indexOf('application/json') < 0) {
+  response.status(500)
+  response.json({error: '请用json请求数据' })
+  return
+}
+
+try {
+    const data = request.body
+    console.log('data: ', data)
+    if (data && typeof(data) == 'object') {
+        if (data.question == "/ping") {
+            response.json({ result: true })
+            return
+        }
+    }
+    response.json({ result: false })
+} catch(err) {
+    response.status(500)
+    response.json({
+      error: err?.toString()
+    })
+    return
+}
